3ee9b4f7473cdecb99c1c67c0cdac83f91b2fe35
robin.correa <robin.correa21@gmail.com>
2024-05-09 18:38:37 +0800

    First commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace asp_dotnet8_mvc_admin.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
